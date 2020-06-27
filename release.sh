bc1384927994b76a17ff1c4134c2dbeca26398c5
md5crypt <md5crypt@users.noreply.github.com>
2020-06-28 01:01:46 +0200

    update scripts

--- release.sh
+++ release.sh
@@ -6,7 +6,8 @@ rm -rf build/release/*
 cp scripts/build/bundle.js build/release/scripts.js
 cp build/engine.js build/release
 cp build/data.json build/release
-cp build/atlas.png build/release
+cp build/fonts.json build/release
+cp build/atlas-* build/release
 cp static/* build/release
 
 rm -rf //192.168.0.1/root/mnt/data/uwuowo/ck/game/*
